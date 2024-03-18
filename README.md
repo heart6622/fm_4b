c67dd10fd0f3c9ea999e39a2ace4835d6cef3475
zh <2582747658@qq.com>
2024-03-18 08:51:39 +0800

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+链接：https://www.yuque.com/heart-rcp14/cxbwmc/kgsfnfs31vt8ukdw?singleDoc# 《用树莓派搭建 FM 广播，播放音乐和实时语音》
---
+++ mailbox.cpp
@@ -0,0 +1,279 @@
+/*
+Copyright (c) 2012, Broadcom Europe Ltd.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holder nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <assert.h>
+#include <stdint.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+
+#include "mailbox.hpp"
+
+#define PAGE_SIZE (4 * 1024)
+// 将指定的内存区域映射到进程的地址空间中，以便可以对物理内存进行访问和操作
+void *mapmem(unsigned base, unsigned size)
+{
+   int mem_fd;
+   unsigned offset = base % PAGE_SIZE;
+   base = base - offset;
+   size = size + offset;
+   /* open /dev/mem */
+   if ((mem_fd = open("/dev/mem", O_RDWR | O_SYNC)) < 0)
+   {
+      printf("can't open /dev/mem\nThis program should be run as root. Try prefixing command with: sudo\n");
+      exit(-1);
+   }
+   void *mem = mmap(
+       0,
+       size,
+       PROT_READ | PROT_WRITE,
+       MAP_SHARED /*|MAP_FIXED*/,
+       mem_fd,
+       base);
+#ifdef DEBUG
+   printf("base=0x%x, mem=%p\n", base, mem);
+#endif
+   if (mem == MAP_FAILED)
+   {
+      printf("mmap error %" PRIdPTR "\n", (intptr_t)mem);
+      exit(-1);
+   }
+   close(mem_fd);
+   return (char *)mem + offset;
+}
+
+void unmapmem(void *addr, unsigned size) // 用于回收指定地址和大小的内存页
+{
+   const intptr_t offset = (intptr_t)addr % PAGE_SIZE;
+   addr = (char *)addr - offset;
+   size = size + offset;
+   int s = munmap(addr, size);
+   if (s != 0)
+   {
+      printf("munmap error %d\n", s);
+      exit(-1);
+   }
+}
+
+/*
+ * use ioctl to send mbox property message
+ 该函数是一个静态函数，名为mbox_property，
+ 它接收三个参数：file_desc是一个整数，表示文件描述符；
+ buf是一个指向任意类型数据的指针。
+ 该函数主要通过ioctl函数设置或获取某个设备的属性。如果ioctl调用失败，则会打印错误信息。
+ 在DEBUG宏被定义的情况下，该函数还会打印buf指向的数据的十六进制表示形式。
+ 最后，该函数返回ioctl函数的返回值。
+ */
+
+static int mbox_property(int file_desc, void *buf)
+{
+   int ret_val = ioctl(file_desc, IOCTL_MBOX_PROPERTY, buf);
+
+   if (ret_val < 0)
+   {
+      printf("ioctl_set_msg failed: %d\n", ret_val);
+   }
+
+#ifdef DEBUG
+   unsigned *p = buf;
+   int i;
+   unsigned size = *(unsigned *)buf;
+   for (i = 0; i < size / 4; i++)
+      printf("%04x: 0x%08x\n", i * sizeof *p, p[i]);
+#endif
+   return ret_val;
+}
+// 从内存中分配指定大小、对齐和标志的内存块。
+unsigned mem_alloc(int file_desc, unsigned size, unsigned align, unsigned flags)
+{
+   int i = 0;
+   unsigned p[32];
+   p[i++] = 0;          // size
+   p[i++] = 0x00000000; // process request
+
+   p[i++] = 0x3000c; // (the tag id)
+   p[i++] = 12;      // (size of the buffer)
+   p[i++] = 12;      // (size of the data)
+   p[i++] = size;    // (num bytes? or pages?)
+   p[i++] = align;   // (alignment)
+   p[i++] = flags;   // (MEM_FLAG_L1_NONALLOCATING)
+
+   p[i++] = 0x00000000;  // end tag
+   p[0] = i * sizeof *p; // actual size
+
+   mbox_property(file_desc, p);
+   return p[5];
+}
+// 该函数用于释放内存，通过给定的文件描述符和句柄与 mbox_property 函数交互来完成内存释放操作。
+unsigned mem_free(int file_desc, unsigned handle)
+{
+   int i = 0;
+   unsigned p[32];
+   p[i++] = 0;          // size
+   p[i++] = 0x00000000; // process request
+
+   p[i++] = 0x3000f; // (the tag id)
+   p[i++] = 4;       // (size of the buffer)
+   p[i++] = 4;       // (size of the data)
+   p[i++] = handle;
+
+   p[i++] = 0x00000000;  // end tag
+   p[0] = i * sizeof *p; // actual size
+
+   mbox_property(file_desc, p);
+   return p[5];
+}
+
+unsigned mem_lock(int file_desc, unsigned handle)
+{
+   int i = 0;
+   unsigned p[32];
+   p[i++] = 0;          // size
+   p[i++] = 0x00000000; // process request
+
+   p[i++] = 0x3000d; // (the tag id)
+   p[i++] = 4;       // (size of the buffer)
+   p[i++] = 4;       // (size of the data)
+   p[i++] = handle;
+
+   p[i++] = 0x00000000;  // end tag
+   p[0] = i * sizeof *p; // actual size
+
+   mbox_property(file_desc, p);
+   return p[5];
+}
+
+unsigned mem_unlock(int file_desc, unsigned handle)
+{
+   int i = 0;
+   unsigned p[32];
+   p[i++] = 0;          // size
+   p[i++] = 0x00000000; // process request
+
+   p[i++] = 0x3000e; // (the tag id)
+   p[i++] = 4;       // (size of the buffer)
+   p[i++] = 4;       // (size of the data)
+   p[i++] = handle;
+
+   p[i++] = 0x00000000;  // end tag
+   p[0] = i * sizeof *p; // actual size
+
+   mbox_property(file_desc, p);
+   return p[5];
+}
+
+unsigned execute_code(int file_desc, unsigned code, unsigned r0, unsigned r1, unsigned r2, unsigned r3, unsigned r4, unsigned r5)
+{
+   int i = 0;
+   unsigned p[32];
+   p[i++] = 0;          // size
+   p[i++] = 0x00000000; // process request
+
+   p[i++] = 0x30010; // (the tag id)
+   p[i++] = 28;      // (size of the buffer)
+   p[i++] = 28;      // (size of the data)
+   p[i++] = code;
+   p[i++] = r0;
+   p[i++] = r1;
+   p[i++] = r2;
+   p[i++] = r3;
+   p[i++] = r4;
+   p[i++] = r5;
+
+   p[i++] = 0x00000000;  // end tag
+   p[0] = i * sizeof *p; // actual size
+
+   mbox_property(file_desc, p);
+   return p[5];
+}
+
+unsigned qpu_enable(int file_desc, unsigned enable)
+{
+   int i = 0;
+   unsigned p[32];
+
+   p[i++] = 0;          // size
+   p[i++] = 0x00000000; // process request
+
+   p[i++] = 0x30012; // (the tag id)
+   p[i++] = 4;       // (size of the buffer)
+   p[i++] = 4;       // (size of the data)
+   p[i++] = enable;
+
+   p[i++] = 0x00000000;  // end tag
+   p[0] = i * sizeof *p; // actual size
+
+   mbox_property(file_desc, p);
+   return p[5];
+}
+
+unsigned execute_qpu(int file_desc, unsigned num_qpus, unsigned control, unsigned noflush, unsigned timeout)
+{
+   int i = 0;
+   unsigned p[32];
+
+   p[i++] = 0;          // size
+   p[i++] = 0x00000000; // process request
+   p[i++] = 0x30011;    // (the tag id)
+   p[i++] = 16;         // (size of the buffer)
+   p[i++] = 16;         // (size of the data)
+   p[i++] = num_qpus;
+   p[i++] = control;
+   p[i++] = noflush;
+   p[i++] = timeout; // ms
+
+   p[i++] = 0x00000000;  // end tag
+   p[0] = i * sizeof *p; // actual size
+
+   mbox_property(file_desc, p);
+   return p[5];
+}
+
+int mbox_open()
+{
+   int file_desc;
+
+   // open a char device file used for communicating with kernel mbox driver
+   file_desc = open(DEVICE_FILE_NAME, 0);
+   if (file_desc < 0)
+   {
+      printf("Can't open device file: %s\n", DEVICE_FILE_NAME);
+      printf("Try creating a device file with: sudo mknod %s c %d 0\n", DEVICE_FILE_NAME, MAJOR_NUM);
+      exit(-1);
+   }
+   return file_desc;
+}
+
+void mbox_close(int file_desc)
+{
+   close(file_desc);
+}
